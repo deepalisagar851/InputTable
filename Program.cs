2f0a6e52ee15c100cab85b2a51ef00d0fcdb82bb
Deepali <96642348+deepalisagar851@users.noreply.github.com>
2022-02-18 15:16:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace MyApp
+{
+    class Program
+    { 
+        static void Table(int num)
+        {
+            for (int i = 1; i<= 10; i++)
+
+                Console.WriteLine(num + "x" + i + "=" + (i * num));
+        }
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Enter the number");
+          int  num = Convert.ToInt32(Console.ReadLine());
+            
+            Table(num);
+
+
+
+
+        }
+
+    }
+}
